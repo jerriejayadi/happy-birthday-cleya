aa3fde4a0c68c00d384250c1c6ec7255928e79d1
jerriejayadi <tjiauw.jerrie@nanovest.io>
2024-08-12 00:44:06 +0800

    INITIATE

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Happy_Monkey, Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const happyMonkey = Happy_Monkey({ subsets: ["latin"], weight: ["400"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={`scroll-smooth`}>
+      <body className={`${inter.className} ${happyMonkey.className}`}>
+        {children}
+      </body>
     </html>
   );
 }
